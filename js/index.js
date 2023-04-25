5055d8033555d5bc994e3cb40562cdf5804d68f1
Garve Hays <gkhays@gmail.com>
2023-04-24 21:51:46 -0500

    Parse additional objects

--- js/index.js
+++ js/index.js
@@ -15,27 +15,55 @@ function analyzeJWT(token) {
   var jwtLines = $('<div class="thin-border"></div>');
   nameBlock.append(jwtLines);
 
+  let div;
+  let span;
+
   for (var key in token) {
-    if (key == "name") {
-      var div = $('<div class="line1"></div>');
-      var span = $('<span class="highlight-green"></span>');
-      console.log("Analyzing line: " + token[key]);
-      span.text(key + ": " + token[key]);
-      div.append(span);
-      jwtLines.append(div);
-    } else if (key == "email") {
-      var div = $('<div class="line2"></div>');
-      var span = $('<span class="highlight-blue"></span>');
-      console.log("Analyzing line: " + token[key]);
-      span.text(key + ": " + token[key]);
-      div.append(span);
-      jwtLines.append(div);
+    switch (key) {
+      case "name":
+        div = $('<div class="line1"></div>');
+        span = $('<span class="highlight-green"></span>');
+        console.log("Analyzing line: " + token[key]);
+        span.text(key + ": " + token[key]);
+        div.append(span);
+        jwtLines.append(div);
+        break;
+      case "email":
+        div = $('<div class="line2"></div>');
+        span = $('<span class="highlight-blue"></span>');
+        console.log("Analyzing line: " + token[key]);
+        span.text(key + ": " + token[key]);
+        div.append(span);
+        jwtLines.append(div);
+        break;
+      case "exp":
+        div = $('<div class="line3"></div>');
+        span = $('<span class="highlight-yellow"></span>');
+        console.log("Analyzing line: " + token[key]);
+        span.text("Expires: " + timeConverter(token[key]));
+        div.append(span);
+        jwtLines.append(div);
+        break;
+      case "nbf":
+        div = $('<div class="line4"></div>');
+        span = $('<span class="highlight-yellow"></span>');
+        console.log("Analyzing line: " + token[key]);
+        span.text("Not Before: " + timeConverter(token[key]));
+        div.append(span);
+        jwtLines.append(div);
+        break;
+      default:
+        console.log("out of keys");
     }
   }
 
   nameBlock.addClass($("#out").attr("class"));
   nameBlock.attr("id", $("#out").attr("id"));
   $("#out").replaceWith(nameBlock);
+}
+
+function timeConverter(timestamp) {
+  return new Date(timestamp * 1000);
 }
 
 $(document).ready(function () {
